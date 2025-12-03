1c996bb124f452ba9aca1eabf09e0b17c4122bf9
Swanand Kolekar <swanand.k17@gmail.com>
2025-12-03 16:31:45 +0530

    contact us page: todo email service

--- raut-gardening/src/app/contact-us/contact-us.component.ts
+++ raut-gardening/src/app/contact-us/contact-us.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
 export class ContactUsComponent {
 
+  @Input() loadedInHome = false;
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) return;
+
+    console.log('Form submitted:', form.value);
+
+    // TODO: Hook this to backend / email API
+    alert('Thank you for contacting us! We will reach out shortly.');
+
+    form.reset();
+  }
 }
