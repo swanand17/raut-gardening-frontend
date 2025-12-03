5be45ee56e9e63a43d347f3d821624a4b61764e2
Swanand Kolekar <swanand.k17@gmail.com>
2025-12-03 16:32:52 +0530

    home update

--- raut-gardening/src/app/home/home.component.ts
+++ raut-gardening/src/app/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { ServicesComponent } from '../services/services.component';
+import { AboutUsComponent } from "../about-us/about-us.component";
+import { ContactUsComponent } from '../contact-us/contact-us.component';
+import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, ServicesComponent],
+  imports: [HeroComponent, ServicesComponent, AboutUsComponent, ContactUsComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
